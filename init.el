fe0ef80b32bfb7469096892e7809bbd8846ebf4b
stevechan <chanshunli@gmail.com>
2018-04-09 16:32:33 +0800

    helm ag

--- init.el
+++ init.el
@@ -75,3 +75,17 @@
 
 (provide 'init-helm)
 
+(define-key global-map (kbd "C-x C-a") 'helm-projectile-ag)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm-ag queue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
