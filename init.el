b2b6a9362b271b17761902df1a90302e0f5fe43e
stevechan <chanshunli@gmail.com>
2018-04-09 15:01:46 +0800

    helm & clj config

--- init.el
+++ init.el
@@ -41,3 +41,37 @@
    (goto-char (point-max))))
 
 (define-key global-map (kbd "C-c k") 'ido-kill-buffer)
+
+(defun clj-eval-sexp (sexp)
+  (interactive "sClJ-EVAL:")
+  (cider-interactive-eval sexp))
+(define-key global-map (kbd "M-\"") 'clj-eval-sexp)
+
+(define-key global-map (kbd "M-g") 'goto-line)
+
+(require 'helm)
+(require 'helm-config)
+
+(helm-mode 1)
+(helm-autoresize-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-s") 'helm-occur)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+
+(setq helm-split-window-in-side-p           t
+      helm-move-to-line-cycle-in-source     t
+      helm-ff-search-library-in-sexp        t
+      helm-M-x-fuzzy-match                  t   ; 模糊搜索
+      helm-buffers-fuzzy-matching           t
+      helm-locate-fuzzy-match               t
+      helm-recentf-fuzzy-match              t
+      helm-scroll-amount                    8
+      helm-ff-file-name-history-use-recentf t)
+
+(provide 'init-helm)
+
